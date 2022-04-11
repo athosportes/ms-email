922ec4d9f5fb166b6a1cc06406e2e68823ea9714
athosportes <athosuti@gmail.com>
2022-04-11 19:00:56 -0300

    first commit

---
+++ src/main/java/com/ms/email/dtos/EmailDto.java
@@ -0,0 +1,29 @@
+package com.ms.email.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class EmailDto {
+
+    @NotBlank
+    private String ownerRef;
+
+    @Email
+    @NotBlank
+    private String emailFrom;
+
+    @Email
+    @NotBlank
+    private String emailTo;
+
+    @NotBlank
+    private String subject;
+
+    @NotBlank
+    private String text;
+}
